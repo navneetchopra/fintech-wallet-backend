04149b39649a29390f700158afcd1b842854d371
Navneet Chopra <nchopra256@gmail.com>
2026-06-18 16:01:05 +0530

    Added CORS support

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express=require("express");
+const cors=require(cors());
 const authRoutes=require("./routes/authRoutes");
 const app=express();
 const pool=require("./config/db");
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth",authRoutes);
 app.get("/", (req, res) => {
